343508e2be9ed7d92515dfbae4e5bfb0af8ff29e
anew318@hotmail.com <anew318@hotmail.com>
2023-12-24 06:31:59 +0800

    yes

--- Model.cs
+++ Model.cs
@@ -1,103 +1,85 @@
-﻿using Microsoft.EntityFrameworkCore;
-using System.ComponentModel.DataAnnotations;
-
-public class RavenloftContext// : DbContext
+﻿public class RavenloftContext// : DbContext
 {
-    public List<Source> Sources { get; set; } = new();
-    public List<Source.Trait> SourceTraits { get; set; } = new();
+    public List<Source> Sources = new();
+    public List<Source.Trait> Editions = new();
+    public List<Source.Trait> Canons = new();
+    public List<Source.Trait> Medias = new();
 
-    public List<Trait> Traits { get; set; } = new();
+    public List<Trait> CampaignSettings = new();
+    public List<Trait> Alignments = new();
+    public List<Trait> Languages = new();
+    public List<Trait> Creatures = new();
 
-    public Dictionary<string, Domain> Domains { get; set; } = new();
-    public Dictionary<string, Location> Locations { get; set; } = new();
-    public Dictionary<string, Item> Items { get; set; } = new();
-    public Dictionary<string, NPC> NPCs { get; set; } = new();
-    public Dictionary<string, Group> Groups { get; set; } = new();
+    public Dictionary<string, Domain> Domains = new();
+    public Dictionary<string, UseVariableName> Locations = new();
+    public Dictionary<string, UseVariableName> Items = new();
+    public Dictionary<string, NPC> NPCs = new();
+    public Dictionary<string, UseVariableName> Groups = new();
 }
 
 public abstract class UseName
 {
-    [Key] public string Key { get; set; } //Key
-    public string ExtraInfo { get; set; } = string.Empty; //external links
+    public string OriginalName { get; set; } //For searching purposes
+    public string ExtraInfo = string.Empty; //external links
 }
 public class Source : UseName
 {
-    public List<Domain> Domains { get; set; } = new();
-    public List<Location> Locations { get; set; } = new();
-    public List<NPC> NPCs { get; set; } = new();
-    public List<Item> Items { get; set; } = new();
-    public List<Group> Groups { get; set; } = new();
-    public List<Trait> Traits { get; set; } = new();
-    public string ReleaseDate { get; set; }
+    public List<InSource<Domain         >> Domains   = new();
+    public List<InSource<UseVariableName>> Locations = new();
+    public List<InSource<NPC            >> NPCs      = new();
+    public List<InSource<UseVariableName>> Items     = new();
+    public List<InSource<UseVariableName>> Groups    = new();
+
+    public Trait Edition, Canon, Media;
+    public string ReleaseDate;
+
     //Levels, contributors, release date
     public class Trait : UseName
     {
-        public string Type { get; set; }
-        public List<Source> Sources { get; set; } = new();
+        public string Type;
+        public List<Source> Sources = new();
     }
 }
-
-public class Trait : UseName, IHasDomains, IHasLocations, IHasItems, IHasNPCs, IHasGroups
+public class InSource<T>
 {
-    public string Type { get; set; }
-    public Dictionary<string, Domain> Domains { get; set; } = new();
-    public Dictionary<string, Location> Locations { get; set; } = new();
-    public Dictionary<string, Item> Items { get; set; } = new();
-    public Dictionary<string, NPC> NPCs { get; set; } = new();
-    public Dictionary<string, Group> Groups { get; set; } = new();
+    public T entity;
+    public Source source;
+    public HashSet<string> Names;
+    public HashSet<Trait> Traits;
+    public string PageNumbers, ExtraInfo;
 }
-
+public class Trait : UseName
+{
+    public List<Domain> Domains = new();
+    public List<UseVariableName> Locations = new();
+    public List<NPC> NPCs = new();
+    public List<UseVariableName> Items = new();
+    public List<UseVariableName> Groups = new();
+}
 public abstract class UseVariableName : UseName
 {
-    public string OriginalName { get; set; } //For searching purposes
-    public Dictionary<string, string> Names { get; set; } = new();//Can be different in different sources
-    public Dictionary<string, Trait> Traits { get; set; } = new();
-    public Dictionary<string, string> PageNumbers { get; set; } = new();
+    public Dictionary<Source, InSource<UseVariableName>> Appearances = new();
+
+    public Dictionary<string, Domain> Domains = new();
+    public Dictionary<string, UseVariableName> Locations = new();
+    public Dictionary<string, UseVariableName> Items = new();
+    public Dictionary<string, NPC> NPCs = new();
+    public Dictionary<string, UseVariableName> Groups = new();
 }
-public class Domain : UseVariableName, IHasLocations, IHasItems, IHasNPCs, IHasGroups
+public class Domain : UseVariableName
 {
-    public Dictionary<string,Location> Locations { get; set; } = new();
-    public Dictionary<string,NPC> NPCs { get; set; } = new();
-    public Dictionary<string,NPC> Darklords { get; set; } = new(); //Convenience
-    public Dictionary<string,Item> Items { get; set; } = new();
-    public Dictionary<string, Group> Groups { get; set; } = new();
-    public Dictionary<string, Group> Clusters { get; set; } = new(); //Convenience
+    public Dictionary<string, NPC> Darklords = new(); //Convenience
+    public Dictionary<string, UseVariableName> Clusters = new(); //Convenience
     //Recommended related media, recorded sessions
 }
-public class Location : UseVariableName, IHasDomains, IHasItems, IHasNPCs, IHasGroups
+public class NPC : UseVariableName
 {
-    public Dictionary<string,Domain> Domains { get; set; } = new();
-    public Dictionary<string,NPC> NPCs { get; set; } = new();
-    public Dictionary<string,Item> Items { get; set; } = new();
-    public Dictionary<string, Group> Groups { get; set; } = new();
-}
-public class NPC : UseVariableName, IHasDomains, IHasLocations, IHasItems, IHasGroups
-{
-    public Dictionary<string, Domain> Domains { get; set; } = new();
-    public Dictionary<string, Location> Locations { get; set; } = new();
-    public Dictionary<string, Item> Items { get; set; } = new();
-    public Dictionary<string, Group> Groups { get; set; } = new();
-    public Dictionary<string, Trait> RelatedTraits { get; set; } = new();
-
-    public Dictionary<string, Relationship> Relationships { get; set; } = new();
+    public Dictionary<string, Trait> RelatedTraits = new();
+    public Dictionary<string, Relationship> Relationships = new();
     public class Relationship
     {
         public NPC Primary { get; set; }
         public NPC Other { get; set; } = new();
         public string RelationshipType { get; set; }
     }
-}
-public class Item : UseVariableName, IHasDomains, IHasLocations, IHasNPCs, IHasGroups
-{
-    public Dictionary<string,Domain> Domains { get; set; } = new();
-    public Dictionary<string,Location> Locations { get; set; } = new();
-    public Dictionary<string,NPC> NPCs { get; set; } = new();
-    public Dictionary<string, Group> Groups { get; set; } = new();
-}
-public class Group : UseVariableName, IHasDomains, IHasLocations, IHasItems, IHasNPCs
-{
-    public Dictionary<string,Domain> Domains { get; set; } = new();
-    public Dictionary<string,NPC> NPCs { get; set; } = new();
-    public Dictionary<string,Location> Locations { get; set; } = new();
-    public Dictionary<string, Item> Items { get; set; } = new();
 }
